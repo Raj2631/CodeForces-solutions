ef0830545f30f6093c5c26a60f2760cec6292985
Rakshit Pathak <rajatpathak435@gmail.com>
2020-05-03 15:24:57 +0530

    Added problems of 800 difficulty

--- 600/1220A. Cards.cpp
+++ 600/1220A. Cards.cpp
@@ -2,22 +2,29 @@
 
 using namespace std;
 
-int main(void) {
+int main(void)
+{
 	int n, countz = 0, countn = 0;
 	string str;
 	cin >> n >> str;
-	for (int i = 0; i < n; i++) {
-		if (str[i] == 'z') {
+	for (int i = 0; i < n; i++)
+	{
+		if (str[i] == 'z')
+		{
 			countz++;
-		} else if (str[i] == 'n') {
+		}
+		else if (str[i] == 'n')
+		{
 			countn++;
 		}
 	}
-	for (int i = 0; i < countn; i++) {
+	for (int i = 0; i < countn; i++)
+	{
 		cout << 1 << ' ';
 	}
 
-	for (int i = 0; i < countz; i++) {
+	for (int i = 0; i < countz; i++)
+	{
 		cout << 0 << ' ';
 	}
 
